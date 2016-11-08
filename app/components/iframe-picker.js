043149dc32843e7fedfec4ea7197268650fe520b
Raido Kuli <raido357@gmail.com>
2016-11-08 09:59:47 +0200

    Fix issue with app dropdown menu resetting to first item
    Remove usage of observer and add selected attribute in the template.
    There was an issue when i selected another app to inspect, it did load the inspector with selected app but the dropdown menu still showed first app as selected.

--- app/components/iframe-picker.js
+++ app/components/iframe-picker.js
@@ -1,6 +1,6 @@
 import Ember from "ember";
-const { Component, computed, run, observer, getOwner } = Ember;
-const { alias } = computed;
+const { Component, computed, run, getOwner } = Ember;
+const { readOnly } = computed;
 
 export default Component.extend({
   model: computed('port.detectedApplications.[]', function() {
@@ -10,31 +10,26 @@ export default Component.extend({
     });
   }),
 
-  selectedApp: alias('port.applicationId'),
-
-  selectedDidChange: observer('selectedApp', function() {
-    // Change iframe being debugged
-    let url = '/';
-    let applicationId = this.get('selectedApp');
-    let list = this.get('port').get('detectedApplications');
-    let app = getOwner(this).lookup('application:main');
-
-    run(app, app.reset);
-    let router = app.__container__.lookup('router:main');
-    let port = app.__container__.lookup('port:main');
-    port.set('applicationId', applicationId);
-    port.set('detectedApplications', list);
-
-    // start
-    app.boot().then(() => {
-      router.location.setURL(url);
-      run(app.__deprecatedInstance__, 'handleURL', url);
-    });
-  }),
+  selectedApp: readOnly('port.applicationId'),
 
   actions: {
     selectIframe(applicationId) {
-      this.set('selectedApp', applicationId);
+      // Change iframe being debugged
+      let url = '/';
+      let list = this.get('port').get('detectedApplications');
+      let app = getOwner(this).lookup('application:main');
+
+      run(app, app.reset);
+      let router = app.__container__.lookup('router:main');
+      let port = app.__container__.lookup('port:main');
+      port.set('applicationId', applicationId);
+      port.set('detectedApplications', list);
+
+      // start
+      app.boot().then(() => {
+        router.location.setURL(url);
+        run(app.__deprecatedInstance__, 'handleURL', url);
+      });
     }
   }
 });
